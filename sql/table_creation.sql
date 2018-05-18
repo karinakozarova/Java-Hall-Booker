ad2d31d8ee9996906270822dbf7068c4c3dd77cb
Kari <karinakozarova1303@gmail.com>
2018-05-18 22:55:38 +0300

    added sql table creation according to our documentation(a.k.a @omchil's excel table)

---
+++ sql/table_creation.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS halls;
+CREATE DATABASE halls CHARSET 'utf8';
+USE halls;
+
+
+CREATE TABLE HallLocation(
+	Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	LocationAdress VARCHAR(1000)
+);
+
+CREATE TABLE RentPeriod(
+	HallId INTEGER NOT NULL  PRIMARY KEY,
+	FromDate DATETIME NOT NULL,
+	UntilDate DATETIME NOT NULL
+);
+
+/*
+http://webarchive.nationalarchives.gov.uk/20100407173424/http://www.cabinetoffice.gov.uk/govtalk/schemasstandards/e-gif/datastandards.aspx
+suggests 35 characters for each of Given Name and Family Name
+*/
+CREATE TABLE BuyerId(
+	Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	FirstName VARCHAR(35),
+	LastName VARCHAR(35)
+);
+
+
+Create table States(
+	Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	StateName VARCHAR(35)
+);
+
+Insert Into States(StateName) Values('Free');
+Insert Into States(StateName) Values('Rented');
+Insert Into States(StateName) Values('Bought');
+
+CREATE TABLE State(
+	Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	StatesId INTEGER NOT NULL,
+    BuyerId INTEGER
+);
+
+CREATE TABLE HallState(
+	HallId INTEGER NOT NULL,
+	StateId INTEGER NOT NULL
+);
+
+CREATE TABLE Halls(
+	Id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	FirstName VARCHAR(250),
+	RentPrice Double,
+    BuyPrice Double,
+    LocationId Integer
+);
