30fb2390461cfbfa23a19c2c1bff743c4e85cf55
Kari <karinakozarova1303@gmail.com>
2018-05-25 17:18:31 +0300

    added functions to be implemented for CRUD operations

--- HallBooker/src/Hall.java
+++ HallBooker/src/Hall.java
@@ -1,8 +1,71 @@
 
 public class Hall {
-
+	
 	public Hall() {
 		// TODO Auto-generated constructor stub
 	}
+		
+	// read 
+	private Integer getId() {
+		return null;
+	}
+	
+	private String getName() {
+		return null;
+		
+	}
+	
+	private Integer getLocationId() {
+		return null;
+		
+	}
+	private double getRentPrice() {
+		double res = 0.0d;
+		return res;
+		
+	}
+	
+	private double getBuyPrice() {
+		double res = 0.0d;
+		return res;
+		
+	}
+	private Integer getHallState(){
+		Integer res = 0;
+		return res;
+		
+	}
+	
+	// create,update
+	private void setId(Integer Id) {
 
+	}
+	
+	private void setName(String name) {
+		
+	}
+	
+	private void setLocationId(Integer Id) {
+		
+	}
+	private void setRentPrice(Double price) {
+
+		
+	}
+	
+	private void setBuyPrice(Double price) {
+
+		
+	}
+	private void setHallState(Integer state){
+
+		
+	}
+	
+	// delete hall
+	private void deleteHall(Integer hallId){
+
+		
+	}
+	
 }
